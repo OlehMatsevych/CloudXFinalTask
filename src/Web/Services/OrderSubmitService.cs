a67e7c03405512848078d05fd18e5578488f9774
Oleh Matsevych <Oleh_Matsevych@epam.com>
2022-07-08 17:53:36 +0300

    feat: cloud support

---
+++ src/Web/Services/OrderSubmitService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.Web.Pages.Basket;
+
+namespace Microsoft.eShopWeb.Web.Services
+{
+    public class OrderSubmitService
+    {
+        private string functionUrl = "https://orderprocessormatsevych.azurewebsites.net/api/Function1?code=AyN25Mv10PzJBQZouLR_ew0xnvAtIHNU5rdwXaJVgBJMAzFusMBRQg==";
+
+        public async Task SendDataAsync(IEnumerable<BasketItemViewModel> items)
+        {
+            var model = new
+            {
+                shippingAddress = new Address("123 Main St.", "Kent", "OH", "United States", "44240"),
+                items = items,
+                finalPrice = items.Sum(x => x.Quantity)
+            };
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var json = JsonSerializer.Serialize(model, options);
+
+            using (var client = new HttpClient())
+            {
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(functionUrl, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(await response.Content.ReadAsStringAsync());
+                }
+            }
+        }
+    }
+}
---
+++ src/Web/Services/ReservationService.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.eShopWeb.Web.ViewModels;
+using Newtonsoft.Json;
+
+namespace Microsoft.eShopWeb.Web.Services
+{
+    public class ReservationService
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://reservationdetails.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9XVm1FOG/U8b2GsGj3Sl/zwDgvXsndm13hZ5FdrT2as=";
+        const string QueueName = "reservations";
+        private IQueueClient queueClient;
+        public ReservationService()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+        }
+
+        public async Task SendToServiceBusAsync(CatalogItemViewModel model)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            var message = new Message();
+            message.Body = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(model));
+
+
+            await queueClient.SendAsync(message);
+        }
+    }
+}
